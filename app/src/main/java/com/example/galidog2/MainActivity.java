ba62a6ed224f71d0695ee0e7cce0936f8479874e
GaetanLeGoelandBleu <neven.touche@gmail.com>
2019-05-28 10:58:50 +0200

    salut à tous

--- app/src/main/java/com/example/galidog2/MainActivity.java
+++ app/src/main/java/com/example/galidog2/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Alban
         //Thibaut
+
+
+        //Baka Konte est largement surcote
     }
 }
