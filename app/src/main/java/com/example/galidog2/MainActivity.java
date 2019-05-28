0b0dcbdca1d9134f9c552846b1d66895c4a444fc
Antoine <antoiine.auger10@gmail.com>
2019-05-28 11:15:36 +0200

    Salut les gars ter

--- app/src/main/java/com/example/galidog2/MainActivity.java
+++ app/src/main/java/com/example/galidog2/MainActivity.java
@@ -10,7 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Bonjour à tous les amis pour ceux qui ne me connaissent pas
-        //Vos grands mères les Netflix qui touchent des aides fixes
     }
 }
