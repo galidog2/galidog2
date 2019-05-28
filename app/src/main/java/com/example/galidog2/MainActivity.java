31801173e650fb962857e84b5295ce586f0ed254
Alban Rahier <ara.rahier@gmail.com>
2019-05-28 16:24:02 +0200

    On créée carto

--- app/src/main/java/com/example/galidog2/MainActivity.java
+++ app/src/main/java/com/example/galidog2/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ici vient une nlle branche dev
+        //ici vient une nlle branche carto
     }
 }
