e9e514e34d1e67a2a33b76ef2ff8ac025ada0b73
Alban Rahier <ara.rahier@gmail.com>
2019-05-28 16:33:02 +0200

    On vérifie que ça marche

--- app/src/main/java/com/example/galidog2/MainActivity.java
+++ app/src/main/java/com/example/galidog2/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //ici vient une nlle branche carto
+        //ça n'a pas marché
     }
 }
