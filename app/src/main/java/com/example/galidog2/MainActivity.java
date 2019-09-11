9b3b9dbf059cd7862ae8bbeefc1ae93ea03db110
haran <ara.rahier@gmail.com>
2019-09-11 15:15:54 +0200

    :lipstick: Change icon launcher

--- app/src/main/java/com/example/galidog2/MainActivity.java
+++ app/src/main/java/com/example/galidog2/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.galidog2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
         // On rend les boutons non cliquables (modes non pris en charge par Galidog2)
         findViewById(R.id.navigation).setClickable(false);
@@ -26,5 +36,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.appbar, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        audioModeStart();
+        SharedPreferences sharedpreferences = getSharedPreferences("Mode",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString("audio", "true");
+        editor.apply();
+        return true;
+    }
+
+    /**
+     * Ce qu'il se passe quand on rentre dans le mode audio
+     */
+    private void audioModeStart() {
+        Toast.makeText(this, "On entre dans le mode audio", Toast.LENGTH_SHORT).show();
+        //TODO : à déplacer dans une activité générique qui implémentera ce dont on a besoin
     }
 }
