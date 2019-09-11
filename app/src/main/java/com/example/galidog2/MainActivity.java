1ab8370e4a9d3ec16ebe9162807826225c6c6c2f
haran <ara.rahier@gmail.com>
2019-09-11 12:21:15 +0200

    :sparkles: Add ChoixMemorisation activity.
    Link it to MainActivity from MainActivity

--- app/src/main/java/com/example/galidog2/MainActivity.java
+++ app/src/main/java/com/example/galidog2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.galidog2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // On rend les boutons non cliquables (modes non pris en charge par Galidog2)
+        findViewById(R.id.navigation).setClickable(false);
+        findViewById(R.id.description).setClickable(false);
+
+        Button memorisationB = (Button)findViewById(R.id.mémorisation);
+        memorisationB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChoixMemorisation.class);
+                startActivity(intent);
+            }
+        });
     }
 }
