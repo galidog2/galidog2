88ba95a6bcb19bf61f1881211afcd7bc3bbfb0cf
haran <ara.rahier@gmail.com>
2019-10-16 17:23:31 +0200

    :bug: Fix the localisation of the user

--- app/src/main/java/com/example/galidog2/MapActivity.java
+++ app/src/main/java/com/example/galidog2/MapActivity.java
@@ -1,6 +1,8 @@
 package com.example.galidog2;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
@@ -11,10 +13,13 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Overlay;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
@@ -30,7 +35,10 @@ public class MapActivity extends AppCompatActivity{
     MapView map = null; // La vue de la map
     private MyLocationNewOverlay myLocationNewOverlay;
     private Switch switchMyLocation; // permet d'activer ou de désactiver l'affichage de la position
-    private final String TAG = "Galidog2";
+    private static final String TAG = "MapActivity";
+    private int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE;
+    private int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION;
+    private int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
 
 
     @Override
@@ -46,16 +54,84 @@ public class MapActivity extends AppCompatActivity{
         switchMyLocation = findViewById(R.id.switchMyLocation);
         miseEnPlaceCarte();
 
+        demandePermissionStockage();
+
+    }
+
+    private void demandePermissionStockage() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // La permission nécessite-t-elle une explication ?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+    }
+
+    private void demandePermissionsLocalisation() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+            }
+        }
+
     }
 
 
     /**
-     * Mise en place de la carte du
+     * Mise en place de la carte
      */
     private void miseEnPlaceCarte() {
         map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setMultiTouchControls(true);
+        map.setBuiltInZoomControls(true);
+        IMapController mapController = map.getController();
+        mapController.setZoom(9.5);
+        GeoPoint startPoint = new GeoPoint(50.6, 3.1);
+        mapController.setCenter(startPoint);
         final List<Overlay> overlays = map.getOverlays();
         ScaleBarOverlay mScaleBarOverlay = new ScaleBarOverlay(map);
         overlays.add(mScaleBarOverlay);
@@ -70,17 +146,18 @@ public class MapActivity extends AppCompatActivity{
      */
     private void miseEnPlaceMyLocationOverlay() {
         myLocationNewOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), map);
-        myLocationNewOverlay.enableFollowLocation();
-        myLocationNewOverlay.enableMyLocation();
+        myLocationNewOverlay.disableMyLocation();
         map.getOverlays().add(myLocationNewOverlay);
         switchMyLocation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    Log.i(TAG, "onCheckedChanged: ");
+                    demandePermissionsLocalisation();
                     myLocationNewOverlay.enableMyLocation();
+                    myLocationNewOverlay.enableFollowLocation();
                 } else {
                     myLocationNewOverlay.disableMyLocation();
+                    myLocationNewOverlay.disableFollowLocation();
                 }
             }
         });
@@ -122,6 +199,4 @@ public class MapActivity extends AppCompatActivity{
         return bitmap;
     }
 }
-
-//Rien
 
