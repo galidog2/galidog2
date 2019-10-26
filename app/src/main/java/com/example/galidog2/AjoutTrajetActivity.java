87fe7a39608c1d2073bc37da4584c14b9967c0f9
haran <ara.rahier@gmail.com>
2019-10-26 16:52:00 +0200

    :sparkles: Création de l'activité d'ajout de trajet

---
+++ app/src/main/java/com/example/galidog2/AjoutTrajetActivity.java
@@ -0,0 +1,14 @@
+package com.example.galidog2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjoutTrajetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_trajet);
+    }
+}
