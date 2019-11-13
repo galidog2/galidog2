284fd9dd31da109c1ba88532fc874eaba47909ad
haran <ara.rahier@gmail.com>
2019-11-13 10:36:17 +0100

    :construction: Add activity of test

---
+++ app/src/main/java/com/example/galidog2/TestNavigationActivity.java
@@ -0,0 +1,14 @@
+package com.example.galidog2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestNavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_navigation);
+    }
+}
