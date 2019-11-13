10a0cfbffc75c0e07fcf134c0ff4ca331e85f12a
haran <ara.rahier@gmail.com>
2019-11-13 16:39:25 +0100

    :construction: Add a demo activity to read routes

--- app/src/main/java/com/example/galidog2/MainActivity.java
+++ app/src/main/java/com/example/galidog2/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,10 +19,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.osmdroid.config.Configuration;
+
 public class MainActivity extends GenericActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(this));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -30,7 +34,7 @@ public class MainActivity extends GenericActivity {
 
         // On rend les boutons non cliquables (modes non pris en charge par Galidog2)
         findViewById(R.id.navigation).setEnabled(false);
-        findViewById(R.id.description).setEnabled(false);
+        /*findViewById(R.id.description).setEnabled(false);*/
 
 
         Button memorisationB = (Button)findViewById(R.id.mémorisation);
@@ -42,6 +46,15 @@ public class MainActivity extends GenericActivity {
                 startActivity(intent);
             }
         });
+        Button descriptionB = (Button)findViewById(R.id.description);
+        descriptionB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TestNavigationActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
