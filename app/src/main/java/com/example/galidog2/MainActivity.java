8ce60309bd11e5da2ca55801d9eac22522e34a5d
IagoRampazzo <iagorampazzo@hotmail.com>
2019-11-24 17:05:30 +0100

    Speech to text avec Google

--- app/src/main/java/com/example/galidog2/MainActivity.java
+++ app/src/main/java/com/example/galidog2/MainActivity.java
@@ -2,9 +2,22 @@ package com.example.galidog2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtOutput;
+    Button speakButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +25,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //ici vient une nlle branche carto
         //ça n'a pas marché
+
+        txtOutput = findViewById(R.id.text);
+        speakButton = findViewById(R.id.speakButton);
+
+        speakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "click",
+                        Toast.LENGTH_SHORT).show();
+                startSpeechToText();
+            }
+        });
     }
+
+
+    private void startSpeechToText() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Speak something...");
+        try {
+            startActivityForResult(intent, 666);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Speech recognition is not supported in this device.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    /**
+     * Callback for speech recognition activity
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 666: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String text = result.get(0);
+                    txtOutput.setText(text);
+                }
+                break;
+            }
+        }
+    }
+
 }
